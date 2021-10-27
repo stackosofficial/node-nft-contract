f390ac223fd5e8b89e6a3b979ec848103e0d2db9
Lopol2010 <evgeniy7@gmail.com>
2021-10-27 16:34:39 +0300

    merge grape

---
+++ contracts/interfaces/StackOSInterface.sol
@@ -0,0 +1,58 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+// TODO: should add getGenerationID ?
+interface StackOSInterface {
+    function getTotalDelegators() 
+        external
+        view
+        returns (
+            uint256
+        );
+
+    function getDelegationTimestamp(
+        uint256 _tokenId
+    )
+        external
+        view
+        returns (
+            uint256
+        );
+
+    function getDelegatee(
+        address _delegate,
+        uint256 _tokenId
+    )
+        external
+        view
+        returns (
+            address
+        );
+
+    function getDelegator(
+        uint256 _tokenId
+    )
+        external
+        view
+        returns (
+            address
+        );
+
+    function balanceOf(
+        address owner
+    )
+        external
+        view
+        returns (
+            uint256 balance
+        );
+
+    function ownerOf(
+        uint256 tokenId
+    ) 
+        external 
+        view 
+        returns (
+            address owner
+        );
+}
