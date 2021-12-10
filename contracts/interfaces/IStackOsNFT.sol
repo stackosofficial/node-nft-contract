b1235c45c4438e16ff3986dea07179a9883528ad
Lopol2010 <evgeniy7@gmail.com>
2021-12-10 11:50:45 +0300

    change interface name casing

---
+++ contracts/interfaces/IStackOsNFT.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+interface IStackOsNFT is IERC721 {
+    function getTotalDelegated() external view returns (uint256);
+
+    function getDelegationTimestamp(uint256 _tokenId)
+        external
+        view
+        returns (uint256);
+
+    function getDelegatee(uint256 _tokenId) external view returns (address);
+
+    function getDelegator(uint256 _tokenId) external view returns (address);
+
+    function transferOwnership(address newOwner) external;
+
+}
